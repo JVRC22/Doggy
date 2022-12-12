f23f70eecece2723a5000400220d8cc92362db48
Javier Resendiz <javier.res220704@gmail.com>
2022-12-11 20:29:59 -0600

    Fragments

--- app/src/main/java/com/example/doggy/admin/AdminActivity.java
+++ app/src/main/java/com/example/doggy/admin/AdminActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.doggy.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,10 +19,17 @@ public class AdminActivity extends AppCompatActivity {
 
     BottomNavigationView navbar;
 
+    UsuariosFragment usuariosFragment = new UsuariosFragment();
+    CasasFragment casasFragment = new CasasFragment();
+    RelacionesFragment relacionesFragment = new RelacionesFragment();
+    ConfiguracionFragment configuracionFragment = new ConfiguracionFragment();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        loadFragment(usuariosFragment);
 
         navbar = findViewById(R.id.navbar);
         navbar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -30,25 +39,34 @@ public class AdminActivity extends AppCompatActivity {
                 switch (item.getItemId())
                 {
                     case R.id.Usuarios:
-                        Toast.makeText(AdminActivity.this, "Usuarios", Toast.LENGTH_SHORT).show();
+                        loadFragment(usuariosFragment);
+                        //Toast.makeText(AdminActivity.this, "Usuarios", Toast.LENGTH_SHORT).show();
                         return true;
 
                     case R.id.Casas:
-                        Toast.makeText(AdminActivity.this, "Casas", Toast.LENGTH_SHORT).show();
+                        loadFragment(casasFragment);
+                        //Toast.makeText(AdminActivity.this, "Casas", Toast.LENGTH_SHORT).show();
                         return true;
 
                     case R.id.Relaciones:
-                        Toast.makeText(AdminActivity.this, "Relaciones", Toast.LENGTH_SHORT).show();
+                        loadFragment(relacionesFragment);
+                        //Toast.makeText(AdminActivity.this, "Relaciones", Toast.LENGTH_SHORT).show();
                         return true;
 
                     case R.id.Configuracion:
-                        Toast.makeText(AdminActivity.this, "Configuracion", Toast.LENGTH_SHORT).show();
+                        loadFragment(configuracionFragment);
+                        //Toast.makeText(AdminActivity.this, "Configuracion", Toast.LENGTH_SHORT).show();
                         return true;
-
                 }
 
                 return false;
             }
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.contenedor, fragment);
+        transaction.commit();
     }
 }
